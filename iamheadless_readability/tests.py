bf7194612bd5bed4a36e8f9753e727c616946bd9
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-16 11:32:34 +0200

    Update tests.py

--- iamheadless_readability/tests.py
+++ iamheadless_readability/tests.py
@@ -1,35 +1,38 @@
 from django.test import TestCase
 
-from . import analyzer
-
+from . import fleish
 
 class ReadabilityTestCase(TestCase):
 
-    text_one = 'This sentence, taken as a reading passage unto itself, is being used to prove a point.'
-    score_one = 69
-
-    text_two = 'The Australian platypus is seemingly a hybrid of a mammal and reptilian creature.'
-    score_two = 37.5
-
-    text_three = 'The cat sat on the mat.'
-    score_three = 116
-
     def test_one(self):
-        calculated_score = round(analyzer.reading_score(self.text_one), 0)
-        self.assertEquals(self.score_one, calculated_score)
+
+        text = ''
+        test_text = 'This sentence, taken as a reading passage unto itself, is being used to prove a point. '
+        for x in '01234567890':
+            text += test_text
+
+        score = 69
+        calculated_score = round(fleish.reading_score(text), 0)
+        self.assertEquals(score, calculated_score)
 
     def test_two(self):
-        calculated_score = round(analyzer.reading_score(self.text_two), 1)
-        self.assertEquals(self.score_two, calculated_score)
+
+        text = ''
+        test_text = 'The Australian platypus is seemingly a hybrid of a mammal and reptilian creature. '
+        for x in '01234567890':
+            text += test_text
+
+        score = 37.5
+        calculated_score = round(fleish.reading_score(text), 1)
+        self.assertEquals(score, calculated_score)
 
     def test_three(self):
-        calculated_score = round(analyzer.reading_score(self.text_three), 0)
-        self.assertEquals(self.score_three, calculated_score)
 
-    def test_four(self):
+        text = ''
+        test_text = 'The cat sat on the mat. '
+        for x in '012345678901234567890012345678900123456789001234567890':
+            text += test_text
 
-        text = self.text_one + ' '
-        text += self.text_two + ' '
-        text += self.text_three
-
-        print(analyzer.analyze(text))
+        score = 116
+        calculated_score = round(fleish.reading_score(text), 0)
+        self.assertEquals(score, calculated_score)
