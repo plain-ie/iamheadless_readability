4f25d0eda995a6391f8fbee3f1295389d65babff
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-28 12:15:23 +0300

    fixed readability viewset

--- iamheadless_readability/viewsets.py
+++ iamheadless_readability/viewsets.py
@@ -4,17 +4,18 @@ from django.core.exceptions import SuspiciousOperation
 from django.shortcuts import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 from iamheadless_publisher_admin import decorators
 
 from . import analyzer
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ReadabilityViewSet(View):
 
     @method_decorator(decorators.login_required(), name='dispatch')
-    def get(self, request):
-
+    def post(self, request):
         request_body = json.loads(request.body.decode('utf-8'))
 
         text = request_body.get('text', None)
