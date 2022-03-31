6c7bf123ed7e51d6ece6128e4684cb7b63262a30
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-31 11:53:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=[
-        "Django==4.0.1",
+        "django>=4.0.2",
         "nltk==3.7",
         "py-readability-metrics==1.4.5",
     ],
